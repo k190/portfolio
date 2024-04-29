4eeee042751f7c6faf2e7a8e209f8afe92120548
k190 <kaleabteferi6@gmail.com>
2024-04-30 02:02:56 +0300

    Step 2

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import { Avatar, Box, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Kaleab!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -15,7 +15,13 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+   <Avatar size="xl" src="https://i.pravatar.cc/150?img=7"/>
+   <Heading as="h2" size="md" color="white" >{greeting}</Heading>
+   <Box  marginTop="10">
+      <Heading marginTop="10"as="h1" size="xl" color="white" >{bio1}</Heading>
+   </Box>
+   
+   <Heading marginTop="10"as="h1" size="xl" color="white">{bio2}</Heading>
   </FullScreenSection>
 );
 
